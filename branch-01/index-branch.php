04accde6e29dd66de43b0ca782612fb632d3ef7c
unknown <prof.agastyo@gmail.com>
2019-04-23 15:03:53 +0700

    testing marge master with branch-01

---
+++ branch-01/index-branch.php
@@ -0,0 +1,15 @@
+<?php
+	echo "hahahaha";
+	$test = "hahahah";
+	print_r($tes);
+
+	for ($i=0; $i < 5; $i++) { 
+		echo "make for loop " . $i . "<br>";
+	}
+	$i = 0;
+	while ($i <= 10) {
+		echo "make while loop " . $i . "";
+		$i++;
+	}
+
+?>
