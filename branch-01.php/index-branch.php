2ea5406316002762926ebff07437362ac9c5ccc2
unknown <prof.agastyo@gmail.com>
2019-04-23 14:54:05 +0700

    add while loop, and add line break to for loop

--- branch-01.php/index-branch.php
+++ branch-01.php/index-branch.php
@@ -4,7 +4,12 @@
 	print_r($tes);
 
 	for ($i=0; $i < 5; $i++) { 
-		echo "make for loop " . $i;
+		echo "make for loop " . $i . "<br>";
+	}
+	$i = 0;
+	while ($i <= 10) {
+		echo "make while loop " . $i . "";
+		$i++;
 	}
 
 ?>
