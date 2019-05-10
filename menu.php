1d0864f025898cb3ee053dd0e3c2a90fa92e4b53
unknown <prof.agastyo@gmail.com>
2019-05-10 09:12:29 +0700

    add menu.php

---
+++ menu.php
@@ -0,0 +1,27 @@
+<?php
+	switch ($menu) {
+		case 'main':
+			$page = "main.php";
+			break;
+		
+		case 'menu1':
+			$page = "menu1.php";
+			break;
+
+		case 'menu2':
+			$page = "menu2.php";
+			break;
+
+		case 'menu3':
+			$page = "menu3.php";
+			break;
+
+		case 'menu4':
+			$page = "menu4.php";
+			break;
+
+		default:
+			$page = "main.php";
+			break;
+	}
+?>
